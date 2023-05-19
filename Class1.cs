d9c1c08f3c09e4f8d4e77dd10988f427bc97fbb6
Yegana Hasanova <yeganamha@code.edu.az>
2023-05-19 11:07:24 -0700

    first task

---
+++ Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Card
+    {
+        public int Code { get; set; }
+        public int CVV { get; set; }
+        public string ValidDate { get; set; }
+        public string FullName { get; set; }
+        public string Bank { get; set; }
+        public decimal Balance { get; set; }
+
+        public void Medaxil(decimal money)
+        {
+            if (money <= 0)
+                return;
+            Balance += money;
+        }
+
+        public void Mexaric(decimal money)
+        { 
+            Balance -= money;   
+        }
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Tasks
+
+{
+
+    internal class Card
+    {
+        public long Code { get; set; }
+        public int CVV { get; set; }
+        public string ValidDate { get; set; }
+        public string FullName { get; set; }
+        public string Bank { get; set; }
+        public decimal Balance { get; set; }
+
+        public void Medaxil(decimal money)
+        {
+            if (money <= 0)
+                return;
+            Balance += money;
+        }
+
+        public void Mexaric(decimal money)
+        {
+            Balance -= money;
+        }
+
+
+        internal class Program
+        {
+            static void Main(string[] args)
+            {
+                Card MyCard = new Card();
+
+                MyCard.Code = 456874563214;
+                MyCard.CVV = 346;
+                MyCard.ValidDate = "30.05.2024";
+                MyCard.FullName = "Yegana Hasanova";
+                MyCard.Bank = "KapitalBank";
+                MyCard.Balance = 120.5m;
+                MyCard.Medaxil(100);
+                MyCard.Mexaric(20.5m);
+
+                Console.WriteLine($" Card Card owner is {MyCard.FullName}");
+                Console.WriteLine($" Bank is {MyCard.Bank}");
+                Console.WriteLine($"Your CVV Code is {MyCard.CVV}");
+                Console.WriteLine($"Your Card Valid Date is {MyCard.ValidDate}");
+                Console.WriteLine($"Your balance is {MyCard.Balance} AZN");
+
+
+
+
+            }
+        }
+    }
+}
